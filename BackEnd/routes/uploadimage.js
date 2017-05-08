f6c216c49564a094bdc25bf22212d73b1e4e576c
shuirong <shuirong1997@icloud.com>
2017-05-08 21:55:55 +0800

    修改上传png之外的图片不成功的问题，后端动态设置图片类型

--- BackEnd/routes/uploadimage.js
+++ BackEnd/routes/uploadimage.js
@@ -10,10 +10,11 @@ router.route('/')
     .post(upload.single('image'), (req, res) => {
         let base64 = req.body.base64;
         //去掉base64数据最前面的＂图片类型＂字符串
+        let type = base64.match(/^data:image\/(.+);/)[1];
         base64 = base64.replace(/^data:image\/\w+;base64,/, "");
         // 解码base64成二进制数据．
         let data = new Buffer(base64, 'base64');
-        const name = `uploads/images/${String(new Date()).replace(/[ :]/g,'').match(/.{6}(.{12})/)[1]}.png`;
+        const name = `uploads/images/${String(new Date()).replace(/[ :]/g,'').match(/.{6}(.{12})/)[1]}.${type}`;
         fs.open(name, "a", 0644, function(e, fd) {
             if (e) throw e;
             fs.write(fd, data, function(e) {
