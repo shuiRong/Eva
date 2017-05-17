e12c8899c63efd5fb93542e6cd1269359ea01499
shuirong <shuirong1997@icloud.com>
2017-05-17 13:09:18 +0800

    完善图片文件夹的相关代码

--- BackEnd/routes/uploadimage.js
+++ BackEnd/routes/uploadimage.js
@@ -19,10 +19,10 @@ router.route('/')
                 }
                 return true;
             }
-            // 如果Home文件夹下，此目录不存在．就新建一个．
-        if (!fsExistsSync('/home/shuirong/uploads/images')) {
-            fs.mkdirSync('/home/shuirong/uploads');
-            fs.mkdirSync('/home/shuirong/uploads/images');
+            // 如果此目录不存在．就新建一个．
+        if (!fsExistsSync('uploads/images')) {
+            fs.mkdirSync('uploads');
+            fs.mkdirSync('uploads/images');
         }
         const name = `uploads/images/${String(new Date()).replace(/[ :]/g,'').match(/.{6}(.{12})/)[1]}.${type}`;
         fs.open(name, "a", 0644, function(e, fd) {
