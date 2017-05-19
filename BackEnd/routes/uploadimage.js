8385fc658f6cf8321f2ec57c74f933a50b337687
shuirong <shuirong1997@icloud.com>
2017-05-19 21:51:26 +0800

    分别测试了base64存储成图片的代码，没问题．又把关闭文件改成了异步．测试

--- BackEnd/routes/uploadimage.js
+++ BackEnd/routes/uploadimage.js
@@ -4,13 +4,6 @@ const fs = require('fs');
 
 router.route('/')
     .post((req, res) => {
-        let base64 = req.body.base64;
-        //去掉base64数据最前面的＂图片类型＂字符串
-        let type = base64.match(/^data:image\/(.+);/)[1];
-        base64 = base64.replace(/^data:image\/\w+;base64,/, "");
-        // 解码base64成二进制数据．
-        let data = new Buffer(base64, 'base64');
-
         let fsExistsSync = (path) => {
             try {
                 fs.accessSync(path, fs.F_OK);
@@ -23,22 +16,33 @@ router.route('/')
             fs.mkdirSync('uploads');
             fs.mkdirSync('uploads/images');
         }
+        let base64 = req.body.base64;
+        //去掉base64数据最前面的＂图片类型＂字符串
+        let type = base64.match(/^data:image\/(.+);/)[1];
+        base64 = base64.replace(/^data:image\/\w+;base64,/, "");
+        // 解码base64成二进制数据．
+        let data = new Buffer(base64, 'base64');
+
         const name = `uploads/images/${String(new Date()).replace(/[ :]/g,'').match(/.{6}(.{12})/)[1]}.${type}`;
         console.log('____________name:', name);
         console.log(base64);
-        res.json({
-            'path': 'temppath',
+
+        fs.open(name, "w+", 0644, function(e, fd) {
+            if (e) throw e;
+            fs.write(fd, data, function(e) {
+                if (e) throw e;
+                fs.close(fd, function(err) {
+                    if (err) {
+                        console.log('Error: close opened image file failed.');
+                        throw err;
+                    }
+                    res.json({
+                        'path': name.replace('uploads', ''),
+                    });
+                });
+
+            });
         });
-        // fs.open(name, "a", 0644, function(e, fd) {
-        //     if (e) throw e;
-        //     fs.write(fd, data, function(e) {
-        //         if (e) throw e;
-        //         fs.closeSync(fd);
-        //         res.json({
-        //             'path': name.replace('uploads', ''),
-        //         });
-        //     });
-        // });
 
     });
 
